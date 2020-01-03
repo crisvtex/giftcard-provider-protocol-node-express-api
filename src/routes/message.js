831059760ce0418c84858459c36c51b1da300610
cristobyte <cristobal.delano@gmail.com>
2020-01-03 12:39:56 -0300

    first commit

--- src/routes/message.js
+++ src/routes/message.js
@@ -4,7 +4,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res) => {
-  return res.send(Object.values(req.context.models.messages));
+  return res.send(Object.values(req.context.models.messages)); 
 });
 
 router.get('/:messageId', (req, res) => {
